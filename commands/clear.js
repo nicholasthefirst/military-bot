ee070216e80e4ffc562b20693bfe5817439ef62d
nicholasthefirst <166142239+nicholasthefirst@users.noreply.github.com>
2026-04-27 09:06:20 -0400

    Create clear.js

---
+++ commands/clear.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('clear')
+        .setDescription('Delete messages')
+        .addIntegerOption(option =>
+            option.setName('amount')
+                .setDescription('Number of messages to delete')
+                .setRequired(true)
+        )
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+
+    async execute(interaction) {
+        const amount = interaction.options.getInteger('amount');
+
+        await interaction.channel.bulkDelete(amount, true);
+
+        await interaction.reply({
+            content: `🧹 Deleted ${amount} messages.`,
+            ephemeral: true
+        });
+    }
+};
